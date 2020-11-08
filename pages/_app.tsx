fdfe304dc4dcfed0e127ae5d8e4f55b6912fe72c
John Letey <johnletey@gmail.com>
2020-11-08 12:29:27 +0000

    feat: light/dark theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
+import { useState, useEffect } from "react";
 import { GeistProvider, CssBaseline } from "@geist-ui/react";
 
 function MyApp({ Component, pageProps }) {
+  const [theme, setTheme] = useState({});
+
+  useEffect(() => {
+    const theme = window.localStorage.getItem("theme");
+    if (theme !== "dark") return;
+    setTheme({ type: "dark" });
+  }, []);
+
   return (
-    <GeistProvider>
+    <GeistProvider theme={theme}>
       <CssBaseline />
       <Component {...pageProps} />
     </GeistProvider>
